7c15a309ae6ef24e96f382420721b12cb3399740
Aurélie Bousquet <aurelie.bousquet@oslandia.com>
2024-11-14 17:36:57 +0100

    Sample graph for contractions hierarchy

--- tools/testers/sampledata.sql
+++ tools/testers/sampledata.sql
@@ -217,3 +217,151 @@ INSERT INTO old_restrictions (rid, to_cost, target_id, via_path) VALUES
 (1, 100, 10,  '7'),
 (2,   4,  9,  '5, 3'),
 (3, 100,  9, '16');
+
+
+
+------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------
+--              SAMPLE DATA
+------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------
+-- CopyRight(c) pgRouting developers
+-- Creative Commons Attribution-Share Alike 3.0 License : https://creativecommons.org/licenses/by-sa/3.0/
+
+DROP TABLE IF EXISTS edges_2;
+DROP TABLE IF EXISTS vertices_2;
+
+create table edges_2 (
+    id bigserial primary key,
+    source bigint,
+    target bigint,
+    cost float,
+    reverse_cost float,
+    x1 float,
+    y1 float,
+    x2 float,
+    y2 float,
+    geom geometry
+);
+
+
+/*---------------------------------------------------------------
+
+ 1_______11_____9_____7
+ | \      \    / \   /|
+ |  \      \  /   \ / |
+ |   3______10_____8  |
+ |  /  \   / |      \ |
+ | /    \ /  |       \|
+ 2/______4___5________6
+
+
+
+A-K = 1-11 = cost 3 ;
+K-I = 11-9 = cost 6 ;
+A-C = 1-3 = cost 5 ;
+A-B = 1-2 = cost 3 ;
+B-D = 2-4 = cost 5 ;
+B-C = 2-3 = cost 3 ;
+C-D = 3-4 = cost 2 ;
+D-E = 4-5 = cost 7 ;
+E-F = 5-6 = cost 6 ;
+C-J = 3-10 = cost 2 ;
+J-D = 10-4 = cost 4 ;
+J-E = 10-5 = cost 3 ;
+J-K = 10-11 = cost 3 ;
+J-I = 10-9 = cost 4 ;
+J-H = 10-8 = cost 2 ;
+H-F = 8-6 = cost 2 ;
+H-G = 8-7 = cost 3 ;
+G-F = 7-6 = cost 4 ;
+I-H = 9-8 = cost 3 ;
+I-G = 9-7 = cost 5 ;
+
+
+1(0,2)
+2(0,0)
+3(0.5,1)
+4(1,0)
+5(1.5,0)
+6(3,0)
+7(3,1)
+8(2.5,1)
+9(2,2)
+10(1.5,1)
+11(1,2)
+
+
+*/
+
+insert into edges_2 (cost, reverse_cost, geom) 
+values
+    ( 3, 3, ST_MakeLine(ST_Point(0,2), ST_Point(1,2)) ), --1-11
+    ( 6, 6, ST_MakeLine(ST_Point(1,2), ST_Point(2,2)) ), --11-9
+    ( 5, 5, ST_MakeLine(ST_Point(0,2), ST_Point(0.5,1)) ), --1-3
+    ( 3, 3, ST_MakeLine(ST_Point(0,2), ST_Point(0,0)) ), --1-2
+    ( 5, 5, ST_MakeLine(ST_Point(0,0), ST_Point(1,0)) ), --2-4
+    ( 3, 3, ST_MakeLine(ST_Point(0,0), ST_Point(0.5,1)) ), --2-3
+    ( 2, 2, ST_MakeLine(ST_Point(0.5,1), ST_Point(1,0)) ), --3-4
+    ( 7, 7, ST_MakeLine(ST_Point(1,0), ST_Point(1.5,0)) ), --4-5
+    ( 6, 6, ST_MakeLine(ST_Point(1.5,0), ST_Point(3,0)) ), --5-6
+    ( 2, 2, ST_MakeLine(ST_Point(0.5,1), ST_Point(1.5,1)) ), --3-10
+    ( 4, 4, ST_MakeLine(ST_Point(1.5,1), ST_Point(1,0)) ), --10-4
+    ( 3, 3, ST_MakeLine(ST_Point(1.5,1), ST_Point(1.5,0)) ), --10-5
+    ( 3, 3, ST_MakeLine(ST_Point(1.5,1), ST_Point(1,2)) ), --10-11
+    ( 4, 4, ST_MakeLine(ST_Point(1.5,1), ST_Point(2,2)) ), --10-9
+    ( 2, 2, ST_MakeLine(ST_Point(1.5,1), ST_Point(2.5,1)) ), --10-8
+    ( 2, 2, ST_MakeLine(ST_Point(2.5,1), ST_Point(3,0)) ), --8-6
+    ( 3, 3, ST_MakeLine(ST_Point(2.5,1), ST_Point(3,1)) ), --8-7
+    ( 4, 4, ST_MakeLine(ST_Point(3,1), ST_Point(3,0)) ), --7-6
+    ( 5, 5, ST_MakeLine(ST_Point(2,2), ST_Point(3,1)) ), --9-7
+    ( 3, 3, ST_MakeLine(ST_Point(2,2), ST_Point(2.5,1)) ) --9-8
+;
+
+/* -- q1 */
+SELECT * INTO vertices_2
+FROM pgr_extractVertices('SELECT id, geom FROM edges_2 ORDER BY id');
+
+/* -- q1-1 */
+CREATE SEQUENCE vertices_2_id_seq;
+
+ALTER TABLE vertices_2
+ALTER COLUMN id SET DEFAULT nextval('vertices_2_id_seq');
+
+ALTER SEQUENCE vertices_2_id_seq
+OWNED BY vertices_2.id;
+
+SELECT setval('vertices_2_id_seq', (SELECT coalesce(max(id)) FROM vertices_2));
+
+/* -- q1-2 */
+\d vertices_2
+
+/* -- q2 */
+SELECT *
+FROM vertices_2;
+
+/* -- q3 */
+/* -- set the source information */
+UPDATE edges_2 AS e
+SET
+    source = v.id,
+    x1 = x,
+    y1 = y
+FROM vertices_2 AS v
+WHERE ST_StartPoint(e.geom) = v.geom;
+
+/* -- set the target information */
+UPDATE edges_2 AS e
+SET 
+    target = v.id,
+    x2 = x,
+    y2 = y
+FROM vertices_2 AS v
+WHERE ST_EndPoint(e.geom) = v.geom;
+
+/* -- q4 */
+SELECT id, source, target
+FROM edges_2
+ORDER BY id;
+/* -- q5 */
+
